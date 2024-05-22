7d7f85bf6ea2cdeeacea61551d4fb7d1422df307
Sanaullah <sanaullahsolangi817@gmail.com>
2024-05-22 19:02:18 +0500

    More improvement is added

--- assets/app.js
+++ assets/app.js
@@ -1,27 +1,36 @@
 // GETTING ELEMENTS FROM HTML
 var car = document.querySelector(".car");
 var redLight = document.querySelector(".redLight");
+var signalTwoRed = document.querySelector(".signalTwoRed");
 var greenLight = document.querySelector(".greenLight");
+var signalTwoGreen = document.querySelector(".signalTwoGreen");
 var yellowLight = document.querySelector(".yellowLight");
-var count = 5;
+var startBtn = document.querySelector(".startBtn");
+var stopBtn = document.querySelector(".stopBtn");
+var count = 1;
 var refId = "";
 function moveCar(event) {
   var ref = setInterval(() => {
-    car.style.left = count + "px";
+    car.style.left = count + "%";
     redLight.style.backgroundColor = "rgb(255, 255, 255, 0.3)";
-    if (count <= 1100) {
-      count += 10;
-    } else if (count > 1120) {
-      stopCar();
-    }
+    setTimeout(() => {
+      if (count <= 75) {
+        count += 1;
+      }
+      if (count >= 75) {
+        stopCar();
+      }
+      if (count >= 65) {
+        signalTwoRed.style.backgroundColor = "red";
+        signalTwoGreen.style.backgroundColor = "rgb(255, 255, 255, 0.3)";
+      }
+    }, 1000);
     console.log(count);
-  }, 50);
-  event.target.style.backgroundColor = "green";
+  }, 100);
+  greenLight.style.backgroundColor = "green";
   refId = ref;
 }
 
 function stopCar() {
-  redLight.style.backgroundColor = "red";
-  greenLight.style.backgroundColor = "rgb(255, 255, 255, 0.3)";
   clearInterval(refId);
 }
